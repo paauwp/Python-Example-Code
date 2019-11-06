66f3ee2f737e74efe5a242cc9bac91057c1262f6
paauwp <39484121+paauwp@users.noreply.github.com>
2019-11-06 14:18:30 +0100

    Add files via upload

--- Listcomprehension.py
+++ Listcomprehension.py
@@ -1,12 +1,11 @@
-import pandas as pd
-
 numbers = [1,2,5,7,9]
 doubled = []
 friends = ["Rolf", "Sam", "Samantha", "Saurabh", "Jen"]
 start_s = []
 
-start_s_comprehension = [friend for friend in friends if friend.startswith("S")] # list comprehension
-doubled_comprehension = [num * 2 for num in numbers] # list comprehension
+# LIST COMPREHENSION
+start_s_comprehension = [friend for friend in friends if friend.startswith("S")]
+doubled_comprehension = [num * 2 for num in numbers] 
 
 # Traditional approach
 for num in numbers:
@@ -17,12 +16,8 @@ for friend in friends:
     if friend.startswith("S"):
         start_s.append(friend)
 
-
+#CHECK OUTPUT
 print(doubled)
 print(doubled_comprehension)
 print(start_s)
 print(start_s_comprehension)
-
-# Convert a list in a dataframe 
-df = pd.DataFrame({'col':friends})
-print (df)
